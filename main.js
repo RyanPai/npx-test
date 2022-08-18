bdcaf9a0cf05844bb989d8437875d4ca53086c61
Ryan Pai <ryan.pai@getoken.io>
2022-08-18 22:56:42 +0800

    feat: add dirname

--- main.js
+++ main.js
@@ -1,3 +1,13 @@
 #!/usr/bin/env node
 
-console.log('yay gist')
+const fs = require('fs');
+const path = require('path')
+console.log(path.join(__dirname, './'))
+
+/* fse.copySync(srcDir, path.resolve('../'), function (err) {
+  if (err) {                 ^
+    console.error(err);      |___{ overwrite: true } // add if you want to replace existing folder or file with same name
+  } else {
+    console.log("success!");
+  }
+}); */
