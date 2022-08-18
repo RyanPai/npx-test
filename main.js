c4d9f726348d1ad7e173bba7bca0061683b87a1f
Ryan Pai <ryan.pai@getoken.io>
2022-08-18 23:01:12 +0800

    feat: path

--- main.js
+++ main.js
@@ -1,9 +1,13 @@
 #!/usr/bin/env node
 
 const fs = require('fs');
-const path = require('path')
-console.log(path.join(__dirname, './'))
+const path = require('path');
 
+const rootDir = path.join(__dirname, '..');
+const packagesDir = path.join(rootDir, 'packages');
+
+console.log(`rootDir = ${rootDir}`)
+console.log(`packagesDir = ${packagesDir}`)
 /* fse.copySync(srcDir, path.resolve('../'), function (err) {
   if (err) {                 ^
     console.error(err);      |___{ overwrite: true } // add if you want to replace existing folder or file with same name
